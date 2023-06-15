bae05fcb9fa53254c28e4d0c55e24924f91b38ee
Daniel Kirby <dkirby24.10@googlemail.com>
2023-06-15 21:24:31 +0100

    Updated backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,9 @@
 export BORG_PASSPHRASE=
 export BORG_REPO=
 
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
 borg prune --list --stats --keep-daily 7 --keep-weekly 4
 
-borg create --list --stats ::cron-{now} ~/docker
+borg create --list --stats ::{now} ~/docker
--- restore.sh
+++ restore.sh
@@ -2,6 +2,9 @@
 export BORG_PASSPHRASE=
 export BORG_REPO=
 
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
 LATEST_ARCHIVE=$(borg list | awk 'END {print $1}')
 
 borg export-tar --list ::$LATEST_ARCHIVE latest.tar.gz
